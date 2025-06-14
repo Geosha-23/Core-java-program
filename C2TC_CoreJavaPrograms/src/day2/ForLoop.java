32a13574848b70022be1b9ebb4a5592f7d4d7e12
Geosha-23 <geoshageo315@gmail.com>
2025-06-14 19:18:02 +0530

    Worked on the program to demonstrate for loop

---
+++ C2TC_CoreJavaPrograms/src/day2/ForLoop.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class ForLoop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=100 ; i++)   
+		//for(int i = 1000; i<=1 ; i--)
+		{	
+			//System.out.print(" i:");
+			System.out.println(i);
+		}
+	}
+}
