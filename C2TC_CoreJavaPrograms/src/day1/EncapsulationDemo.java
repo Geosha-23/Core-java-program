196b09d2f34c80aa30976c2b72f05e5a54bf40fa
Geosha-23 <geoshageo315@gmail.com>
2025-06-15 16:53:56 +0530

    Worked on the program to demonstrate class and object

---
+++ C2TC_CoreJavaPrograms/src/day1/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(23);
+		obj.setName("Geosha");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
