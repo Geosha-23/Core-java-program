4cdaa97f053f66a8e5751407668799cd62e9a7b8
Geosha-23 <geoshageo315@gmail.com>
2025-06-21 12:54:12 +0530

    Worked on the program to demonstrate Single Inheritance

---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day5/singleinheritance/SLInheritance.java
@@ -0,0 +1,10 @@
+package com.tnsif.day5.singleinheritance;
+//driver program for person and trainer entity class
+public class SLInheritance {
+
+	public static void main(String[] args) {
+		Trainer t1 = new Trainer(27,"Grace","KanyaKumari","Program-Trainer","Chennai" );
+        System.out.println(t1);
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day5/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.day5.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student student = new Student("Geoshhh", "319951786180", "Bangalore", 9888878787L, 23, "Agni College");
+		System.out.println(student);
+
+	}
+
+}
