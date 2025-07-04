09edd51ef541f88b6a5a9db3fe134adbb341101a
Geosha-23 <geoshageo315@gmail.com>
2025-07-04 09:44:33 +0530

    Worked on the program to demonstrate the extending interface

---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day8/interfaces/extendinginterfaces/ExtendingInterfaceDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.day8.interfaces.extendinginterfaces;
+
+public class ExtendingInterfaceDemo implements ChildInterface{
+
+	@Override
+	public void print() {
+		System.out.println("print method");
+		
+	}
+
+	@Override
+	public void show() {
+		System.out.println("show method");
+		
+	}
+
+}
+
+
